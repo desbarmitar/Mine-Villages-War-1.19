21397c1915ecf846e5715ab73c70fa8898540b79
Desbarmitar <marcosrgr93@gmail.com>
2022-06-14 00:33:37 +0100

    Initial commit

---
+++ src/main/java/net/desbarmitar/minevillageswar/MineVillagesWarClient.java
@@ -0,0 +1,10 @@
+package net.desbarmitar.minevillageswar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MineVillagesWarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
